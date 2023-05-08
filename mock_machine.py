1854a983206da2bf6284fa1e94304b6bc43aaf13
Andrew Leech <andrew.leech@planetinnovation.com.au>
2023-05-08 12:10:40 +1000

    Re-order classes alphabetically

--- mock_machine.py
+++ mock_machine.py
@@ -26,105 +26,45 @@ import micropython
 # pylint: disable=unused-argument
 
 
-class I2CDevice:
+class ADC:
     """
-    A single I2C device added to a mock_machine.I2C bus.
+    Unittest support class for machine.ADC.
+
+    Modified from radiata/src/firmware/test/mocks.py
     """
 
-    def __init__(self, addr, i2c):
-        self.addr = addr
+    def __init__(self, pin):
+        self.pin = pin
+        self.value_u16 = 0
 
-        # Dict of buffers used for addressed operations
-        self.register_values = {}
-
-        # A buffer used for non-addressed reads
-        self.readbuf = bytes()
-
-        # Add self to I2C
-        i2c.add_device(self)
-
-    # Standard bus operations
-    def readfrom(self, nbytes, stop=True):
+    # Methods
+    def init(self, *, sample_ns, atten):
         """
-        Read nbytes from the peripheral.
+        Apply the given settings to the ADC.
 
-        Returns a bytes object with the data read.
+        Only those arguments that are specified will be changed.
         """
-        if len(self.readbuf) < nbytes:
-            raise ValueError(f"Insufficient bytes to read {nbytes=} with readfrom()")
-        buf = self.readbuf[:nbytes]
-        return buf
 
-    def readfrom_into(self, buf, stop=True):
+    def block(self):
         """
-        Read into buf from the peripheral.
-
-        The number of bytes read will be the length of buf.
-
-        The method returns None.
+        Return the ADCBlock instance associated with this ADC object.
         """
-        if len(self.readbuf) < len(buf):
-            raise ValueError(f"Insufficient bytes to read {len(buf)} with readfrom_into()")
-        buf[:] = self.readbuf[: len(buf)]
 
-    @staticmethod
-    def writeto(buf, stop=True):
+    def read_u16(self):
         """
-        Write the bytes from buf to the peripheral.
+        Take an analog reading and return an integer in the range 0-65535.
 
-        If a NACK is received following the write of a byte from buf then the remaining bytes are
-        not sent.
-
-        If `writeto` is required for a peripheral, then this function will need to be overridden
-        with desired functionality. Otherwise this function defaults to ACK for all bytes.
-
-        The function returns the number of ACKs that were received.
+        The return value represents the raw reading taken by the ADC.
         """
-        return len(buf)
+        return self.value_u16
 
-    # Memory operations
-    def readfrom_mem(self, memaddr, nbytes):
+    def read_uv(self):
         """
-        Read nbytes from the peripheral starting at memaddr.
+        Take an analog reading and return an integer value with units of microvolts.
 
-        Returns a bytes object with the data read.
+        It is up to the particular port whether or not this value is calibrated, and how
+        calibration is done.
         """
-        if memaddr not in self.register_values:
-            raise IndexError(
-                f"Unknown memory address {memaddr=}",
-            )
-        if len(self.register_values[memaddr]) < nbytes:
-            raise ValueError(f"Insufficient bytes to read {nbytes=} from {memaddr=}")
-        buf = self.register_values[memaddr][:nbytes]
-        return buf
-
-    def readfrom_mem_into(self, memaddr, buf):
-        """
-        Read into buf from the peripheral specified by addr, starting at memaddr.
-
-        The number of bytes read is the length of buf. The argument addrsize specifies the address
-        size in bits.
-
-        The method returns None.
-        """
-        if memaddr not in self.register_values:
-            raise IndexError(
-                f"Unknown memory address {memaddr=:x}",
-            )
-        if len(self.register_values[memaddr]) < len(buf):
-            raise ValueError(f"Insufficient bytes to read {len(buf)=} from {memaddr=}")
-        buf[:] = self.register_values[memaddr][: len(buf)]
-
-    def writeto_mem(self, memaddr, buf):
-        """
-        Write buf to the peripheral specified by addr, starting at memaddr.
-
-        The number of bytes written is the length of buf. The argument addrsize specifies the
-        address size in bits.
-
-        The method returns None.
-        """
-        self.register_values[memaddr] = buf
 
 
 class I2C:
@@ -260,6 +200,169 @@ class I2C:
         self.devices[addr].writeto_mem(memaddr, buf)
 
 
+class I2CDevice:
+    """
+    A single I2C device added to a mock_machine.I2C bus.
+    """
+
+    def __init__(self, addr, i2c):
+        self.addr = addr
+
+        # Dict of buffers used for addressed operations
+        self.register_values = {}
+
+        # A buffer used for non-addressed reads
+        self.readbuf = bytes()
+
+        # Add self to I2C
+        i2c.add_device(self)
+
+    # Standard bus operations
+    def readfrom(self, nbytes, stop=True):
+        """
+        Read nbytes from the peripheral.
+
+        Returns a bytes object with the data read.
+        """
+        if len(self.readbuf) < nbytes:
+            raise ValueError(f"Insufficient bytes to read {nbytes=} with readfrom()")
+        buf = self.readbuf[:nbytes]
+        return buf
+
+    def readfrom_into(self, buf, stop=True):
+        """
+        Read into buf from the peripheral.
+
+        The number of bytes read will be the length of buf.
+
+        The method returns None.
+        """
+        if len(self.readbuf) < len(buf):
+            raise ValueError(f"Insufficient bytes to read {len(buf)} with readfrom_into()")
+        buf[:] = self.readbuf[: len(buf)]
+
+    @staticmethod
+    def writeto(buf, stop=True):
+        """
+        Write the bytes from buf to the peripheral.
+
+        If a NACK is received following the write of a byte from buf then the remaining bytes are
+        not sent.
+
+        If `writeto` is required for a peripheral, then this function will need to be overridden
+        with desired functionality. Otherwise this function defaults to ACK for all bytes.
+
+        The function returns the number of ACKs that were received.
+        """
+        return len(buf)
+
+    # Memory operations
+    def readfrom_mem(self, memaddr, nbytes):
+        """
+        Read nbytes from the peripheral starting at memaddr.
+
+        Returns a bytes object with the data read.
+        """
+        if memaddr not in self.register_values:
+            raise IndexError(
+                f"Unknown memory address {memaddr=}",
+            )
+        if len(self.register_values[memaddr]) < nbytes:
+            raise ValueError(f"Insufficient bytes to read {nbytes=} from {memaddr=}")
+        buf = self.register_values[memaddr][:nbytes]
+        return buf
+
+    def readfrom_mem_into(self, memaddr, buf):
+        """
+        Read into buf from the peripheral specified by addr, starting at memaddr.
+
+        The number of bytes read is the length of buf. The argument addrsize specifies the address
+        size in bits.
+
+        The method returns None.
+        """
+        if memaddr not in self.register_values:
+            raise IndexError(
+                f"Unknown memory address {memaddr=:x}",
+            )
+        if len(self.register_values[memaddr]) < len(buf):
+            raise ValueError(f"Insufficient bytes to read {len(buf)=} from {memaddr=}")
+        buf[:] = self.register_values[memaddr][: len(buf)]
+
+    def writeto_mem(self, memaddr, buf):
+        """
+        Write buf to the peripheral specified by addr, starting at memaddr.
+
+        The number of bytes written is the length of buf. The argument addrsize specifies the
+        address size in bits.
+
+        The method returns None.
+        """
+        self.register_values[memaddr] = buf
+
+
+class Pin:
+    """
+    Unittest support class for machine.Pin
+
+    Allows manual setting of input or output pin's value.
+
+    Taken from radiata/src/firmware/test/mocks.py
+    """
+
+    IN = 0
+    OUT = 1
+    ALT = 2
+
+    IRQ_RISING = 269549568
+    IRQ_FALLING = 270598144
+
+    def __init__(self, _, mode=0, pull=0):
+        self._value = 0
+        self._mode = mode
+        self._pull = pull
+        self._alt = None
+        self._irq_handler = None
+        self._irq_trigger = None
+
+    def init(self, mode=0, pull=0, alt=0):
+        self._mode = mode
+        self._pull = pull
+        self._alt = alt
+
+    def value(self, new_value=None):
+        if new_value is not None:
+            old_value = self._value
+            self._value = int(new_value)
+
+            if self._irq_trigger and self._irq_handler:
+                if self._irq_trigger & self.IRQ_RISING and self._value > old_value:
+                    micropython.schedule(self._irq_handler, self)
+                if self._irq_trigger & self.IRQ_FALLING and self._value < old_value:
+                    micropython.schedule(self._irq_handler, self)
+            return None
+        return self._value
+
+    def on(self):
+        self.value(1)
+
+    def off(self):
+        self.value(0)
+
+    def irq(
+        self, handler=None, trigger=IRQ_FALLING | IRQ_RISING, priority=1, wake=None, hard=False
+    ):
+        self._irq_handler = handler
+        self._irq_trigger = trigger
+        return self  # non-standard return value
+
+    def mode(self):
+        return self._mode
+
+    def __call__(self, x=None):
+        return self.value(x)
+
+
 class SPI:
     """
     Unittest support class for machine.SPI
@@ -328,107 +431,4 @@ class SPI:
         The buffers can be the same or different, but both buffers must have the same length.
 
         Returns None.
-        """
-
-
-class Pin:
-    """
-    Unittest support class for machine.Pin
-
-    Allows manual setting of input or output pin's value.
-
-    Taken from radiata/src/firmware/test/mocks.py
-    """
-
-    IN = 0
-    OUT = 1
-    ALT = 2
-
-    IRQ_RISING = 269549568
-    IRQ_FALLING = 270598144
-
-    def __init__(self, _, mode=0, pull=0):
-        self._value = 0
-        self._mode = mode
-        self._pull = pull
-        self._alt = None
-        self._irq_handler = None
-        self._irq_trigger = None
-
-    def init(self, mode=0, pull=0, alt=0):
-        self._mode = mode
-        self._pull = pull
-        self._alt = alt
-
-    def value(self, new_value=None):
-        if new_value is not None:
-            old_value = self._value
-            self._value = int(new_value)
-
-            if self._irq_trigger and self._irq_handler:
-                if self._irq_trigger & self.IRQ_RISING and self._value > old_value:
-                    micropython.schedule(self._irq_handler, self)
-                if self._irq_trigger & self.IRQ_FALLING and self._value < old_value:
-                    micropython.schedule(self._irq_handler, self)
-            return None
-        return self._value
-
-    def on(self):
-        self.value(1)
-
-    def off(self):
-        self.value(0)
-
-    def irq(
-        self, handler=None, trigger=IRQ_FALLING | IRQ_RISING, priority=1, wake=None, hard=False
-    ):
-        self._irq_handler = handler
-        self._irq_trigger = trigger
-        return self  # non-standard return value
-
-    def mode(self):
-        return self._mode
-
-    def __call__(self, x=None):
-        return self.value(x)
-
-
-class ADC:
-    """
-    Unittest support class for machine.ADC.
-
-    Modified from radiata/src/firmware/test/mocks.py
-    """
-
-    def __init__(self, pin):
-        self.pin = pin
-        self.value_u16 = 0
-
-    # Methods
-    def init(self, *, sample_ns, atten):
-        """
-        Apply the given settings to the ADC.
-
-        Only those arguments that are specified will be changed.
-        """
-
-    def block(self):
-        """
-        Return the ADCBlock instance associated with this ADC object.
-        """
-
-    def read_u16(self):
-        """
-        Take an analog reading and return an integer in the range 0-65535.
-
-        The return value represents the raw reading taken by the ADC.
-        """
-        return self.value_u16
-
-    def read_uv(self):
-        """
-        Take an analog reading and return an integer value with units of microvolts.
-
-        It is up to the particular port whether or not this value is calibrated, and how
-        calibration is done.
         """
